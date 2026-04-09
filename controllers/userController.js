2df1eff4de9b14ebab2232ba9b7dbd05de5690bd
satyaprakash121122@gmail.com <satyaprakash121122@gmail.com>
2026-04-09 14:59:07 +0530

    profile api

--- controllers/userController.js
+++ controllers/userController.js
@@ -42,7 +42,22 @@ export const registerUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+// 🔵 Get User Profile
+export const getUserProfile = async (req, res) => {
+    try {
+        if (!req.user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
 
+        res.json({
+            _id: req.user._id,
+            name: req.user.name,
+            email: req.user.email
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 // 🔵 Login User
 export const loginUser = async (req, res) => {
     try {
