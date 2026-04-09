dbc48ba23bf90bbbea8717ad781a1f3e0d134a1a
satyaprakash121122@gmail.com <satyaprakash121122@gmail.com>
2026-04-09 14:07:41 +0530

    commit new

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+
+// routes
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
